399e433a3eca1dec699ce4a018da3cf1c69a3fb8
simonelupone <simone.lupone@gmail.com>
2025-03-31 17:22:50 +0200

    [feat]: add function to get obj values and put into a dom element

--- js/main.js
+++ js/main.js
@@ -46,3 +46,34 @@ const role = document.getElementById('role');
 // console.log(role);
 const mail = document.getElementById('mail');
 // console.log(mail);
+
+// funzione per generare la card
+
+/**
+ * 
+ * function to create a card
+ *  
+ * @param {object} member - the object we want to take data from
+ * @return {string} - dom element
+ * 
+ */
+const createMemberCard = (member) => {
+    const card = `<div class="my-card d-flex gap-4 border bg-dark" style="max-width: 540px;">
+
+                    <div class="img-fluid ">
+                        <img id="card-img" src="./${member.img}" alt="">
+                    </div>
+
+                    <div class="d-flex flex-column gap-3 mt-2">
+                        <div id="name" class="fw-bold fs-4 text-light">${member.name}</div>
+                        <div id="role" class="fs-5 text-light">${member.role}</div>
+                        <div id="mail" class="fs-5"><a
+                                class="text-info link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"" href="">${member.email}</a></div>
+                </div>`;
+
+    console.log(card);
+}
+
+for (let i = 0; i < teamMembers.length; i++) {
+    createMemberCard(teamMembers[i]);
+}
